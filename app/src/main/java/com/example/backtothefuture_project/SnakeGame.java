a1fcabcf947696771a4fa92a832f63197d5d3c55
Pedro <pedrodias3737@gmail.com>
2022-10-26 18:38:44 -0300

    Implementacao da Segunda Tela

--- app/src/main/java/com/example/backtothefuture_project/SnakeGame.java
+++ app/src/main/java/com/example/backtothefuture_project/SnakeGame.java
@@ -2,23 +2,16 @@ package com.example.backtothefuture_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class SnakeGame extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Para maximizar a tela e esconder a barra de notificações.
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_snake_game);
     }
 
-    public void startSnake(View view){
-        Intent intent = new Intent (this,SnakeGame.class);
-        startActivity(intent);
-    }
 }
