4ae5e224fd9e9a414310851b8a5b8414e9bfa47e
Pedro <pedrodias3737@gmail.com>
2022-10-26 16:13:55 -0300

    Implementação da Tela Principal

--- app/src/main/java/com/example/backtothefuture_project/MainActivity.java
+++ app/src/main/java/com/example/backtothefuture_project/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.backtothefuture_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Para maximizar a tela e esconder a barra de notificações.
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
